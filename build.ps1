c8606bc0a14cf7363bbf1a40dcfd6d040d6f607d
Nikolche Kolev <nikolev@microsoft.com>
2021-11-19 13:55:31 -0800

    Cleanup

--- build.ps1
+++ build.ps1
@@ -2,4 +2,4 @@ dotnet pack .\B /p:PackageVersion="1.0.0"
 dotnet pack .\B /p:PackageVersion="2.0.0"
 dotnet pack .\C /p:PackageVersion="1.0.0"
 dotnet pack .\A /p:PackageVersion="1.0.0"
-
+dotnet restore .\consoleapp\consoleapp.csproj
